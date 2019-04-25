326e4168ee9ee422896cdf92a4c8131ecc4b7d54
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-04-24 19:42:04 -0500

    Person and Card

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-    
+
 #include <iostream>
 #include <string>
 #include "Person.cpp"
+#include "Card.cpp"
 
 using namespace std;
 
@@ -9,13 +10,19 @@ int main(){
 	
 	string player1;
 	char hors = 'h';
+	string suites[] = { "Diamonds", "Clubs", "Spades", "Hearts" };
+	Card deck[52];
 	Person dealer;
 	Person player;
+	
+	//Create users
 	dealer.set_name("Dealer");
-
 	cout << "Enter the name of your player: ";
 	cin >> player1;
 	player.set_name(player1);
+	
+	//Populate deck of cards
+
 	
 	cout << "Dealer has a A of Diamonds, and a 5 of Clubs" << endl;
 	cout << player.get_name() << " has a 8 of Spades, and a K of Hearts" << endl;
